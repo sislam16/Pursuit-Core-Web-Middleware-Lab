51d911de13b7dceb5da8ee7c987d4c29453643ed
Suzette <suzetteislam@gmail.com>
2019-10-14 21:06:34 -0400

    files created

---
+++ middlewareserver.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 2300;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+const isAnimal = (req, res, next) => {
+    let animal = req.query.animal
+    const registeredAnimals = ['lion','turtle','jellyfish','shark','penguin','cow','goat']
+    for(let animal of registeredAnimals ) {
+        if(registeredAnimals.includes(animal)){
+            res.json({
+                status: "success",
+                message: true
+            })
+            return;
+        } else {
+            next();
+        }
+    }
+}
+
+const unavailableAnimal = (req, res, next) => {
+    res.json({
+        status: "Fail",
+        message: false
+    })
+}
+app.get('/animal', isAnimal, unavailableAnimal);
+
+app.get('/random',);
+
+
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`)
+})
+
